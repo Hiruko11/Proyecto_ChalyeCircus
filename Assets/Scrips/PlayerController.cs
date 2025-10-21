3db0236d01c19c96fe4786ad6ca779cab8fc889f
Hiruko11 <andresdemond@gmail.com>
2025-10-20 21:46:41 -0500

    MovimientoHorizontalConEspejo

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -1,16 +1,41 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody2D playerRb;
+    PlayerInput playerInput;
+
+    [Range(0f, 5f)]
+
+    public float Speed;
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+
     {
-        
+        PlayerMovement();
+    }
+    
+    void PlayerMovement()
+    {
+        Vector2 moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+        playerRb.linearVelocity = new Vector2(moveInput.x * Speed, playerRb.linearVelocity.y);
+
+        if (moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 }
